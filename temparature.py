291f311c932ba65e3234848602f4e3e2bda83bfd
= <nVidiaPriyadarshini@gmail.com>
2020-08-18 18:25:12 +0530

    Chapter 3 exercises

---
+++ temparature.py
@@ -0,0 +1,5 @@
+# Convert celcius to farenheit based on formula : (0 C * 9/5) + 32 = 32 F
+print("Let's convert 97.0 degree celcius to farenheit")
+
+print("Farenheit conversion is:", (97.0 * 9/5) + 32.0) 
+
